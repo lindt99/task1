bb20893f60e243d359ce35f20eb951ff290c4fd3
本田彩 <ayahonda@ld10-AyaHonda-MacBook-Air.local>
2018-12-31 16:56:45 +0900

    Initial Commit

---
+++ Todo1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todo1
+//
+//  Created by 本田彩 on 2018/12/31.
+//  Copyright © 2018年 本田彩. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
